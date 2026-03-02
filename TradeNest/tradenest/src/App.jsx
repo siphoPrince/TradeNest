c3a2d1a18a0648b385dd9916043384f64fb39d4f
siphoPrince <mabirimisesipho@gmail.com>
2026-03-02 22:19:09 +0200

    push

---
+++ TradeNest/tradenest/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return(
+    <div>Hello sipho lets get it its our year</div>
+  )
+}
+
+export default App
