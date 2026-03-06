9779d0355a417ffa5cfcf84cd3297bdd9a3dff16
siphoPrince <mabirimisesipho@gmail.com>
2026-03-06 12:55:59 +0200

    cleanup comment

--- TradeNest/tradenest/src/components/Engagement.jsx
+++ TradeNest/tradenest/src/components/Engagement.jsx
@@ -1,13 +1,15 @@
-import { Heart, MessageCircleMore,ExternalLink   } from 'lucide-react';
+import { Heart, MessageCircleMore,ExternalLink,Bookmark,UserPen  } from 'lucide-react';
 
 const Engagement = () => {
     return(
         <div className="engagement">
-            <div className="container">
+                <a href="" className="Profile"><UserPen/></a>
                 <a href="" className="like"><Heart/></a>
                 <a href="" className="Comment"><MessageCircleMore /></a>
                 <a href="" className="Share"><ExternalLink /></a>
-            </div>
+                <a href="" className="Save"><Bookmark  /></a>
         </div>
     );
 }
+
+export default Engagement;
--- TradeNest/tradenest/src/components/Feed.jsx
+++ TradeNest/tradenest/src/components/Feed.jsx
@@ -1,19 +1,31 @@
+import { useState } from "react";
+import Engagement from "./Engagement";
+
 const Feed = () =>{
+
+    const [showComments, setShowComments] = useState(false);
     return(
         <div className="feed">
-        <h6 className="feed-title">FlipFeed</h6>
-        <div className="feed-layout">
-            {/* CENTER VIDEO AREA */}
-            <div className="main-screen">
-            video feed
+            <h6 className="feed-title">FlipFeed</h6>
+            <div className="feed-layout">
+                {/* CENTER VIDEO AREA */}
+                <div className="main-screen">
+                video feed
+                <Engagement />
+                </div>
+
+                {/* RIGHT COMMENT PANEL */}
+                {showComments && (
+                    <div className="comment-panel">
+                        <button onClick={() => setShowComments(false)}>
+                            Close
+                        </button>
+
+                        comments will go here
+                    </div>
+                )}
+
             </div>
-
-        {/* RIGHT COMMENT PANEL */}
-        <div className="comment-panel">
-        comments will go here
-        </div>
-
-        </div>
 
         </div>
     );
