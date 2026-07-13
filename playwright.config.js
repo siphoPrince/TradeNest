036a20c564220d0f64fe54d13d9c37081112258f
siphoPrince <mabirimisesipho@gmail.com>
2026-07-13 14:01:48 +0200

    activiating the run start command in webserver

--- playwright.config.js
+++ playwright.config.js
@@ -63,10 +63,10 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://localhost:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command: 'npm run start',
+    url: 'http://localhost:51817',
+    reuseExistingServer: !process.env.CI,
+  },
 });
 
